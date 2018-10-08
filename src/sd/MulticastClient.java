10df3b8260a2e118353f4368b07f239c9780af3b
jpgoncalves97 <jpmg411@gmail.com>
2018-10-08 17:22:02 +0100

    Separar multicast em client e server

--- src/sd/MulticastClient.java
+++ src/sd/MulticastClient.java
@@ -1,33 +1,18 @@
 package sd;
 
-import java.net.MulticastSocket;
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
-import java.io.IOException;
-import java.util.Scanner;
+import java.net.MulticastSocket;
 
-/**
- * The MulticastClient class joins a multicast group and loops receiving
- * messages from that group. The client also runs a MulticastUser thread that
- * loops reading a string from the keyboard and multicasting it to the group.
- * <p>
- * The example IPv4 address chosen may require you to use a VM option to
- * prefer IPv4 (if your operating system uses IPv6 sockets by default).
- * <p>
- * Usage: java -Djava.net.preferIPv4Stack=true MulticastClient
- *
- * @author Raul Barbosa
- * @version 1.0
- */
-public class MulticastClient extends Thread {
-    private String MULTICAST_ADDRESS = "224.0.224.0";
-    private int PORT = 4321;
+class MulticastClient extends Thread {
+
+    private static String MULTICAST_ADDRESS = "224.0.224.0";
+    private static int PORT = 4321;
 
     public static void main(String[] args) {
         MulticastClient client = new MulticastClient();
         client.start();
-        MulticastUser user = new MulticastUser();
-        user.start();
     }
 
     public void run() {
@@ -41,39 +26,11 @@ public class MulticastClient extends Thread {
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
                 socket.receive(packet);
 
-                System.out.println("Received packet from " + packet.getAddress().getHostAddress() + ":" + packet.getPort() + " with message:");
-                String message = new String(packet.getData(), 0, packet.getLength());
-                System.out.println(message);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            socket.close();
-        }
-    }
-}
-
-class MulticastUser extends Thread {
-    private String MULTICAST_ADDRESS = "224.0.224.0";
-    private int PORT = 4321;
-
-    public MulticastUser() {
-        super("User " + (long) (Math.random() * 1000));
-    }
-
-    public void run() {
-        MulticastSocket socket = null;
-        System.out.println(this.getName() + " ready...");
-        try {
-            socket = new MulticastSocket();  // create socket without binding it (only for sending)
-            Scanner keyboardScanner = new Scanner(System.in);
-            while (true) {
-                String readKeyboard = keyboardScanner.nextLine();
-                byte[] buffer = readKeyboard.getBytes();
-
-                InetAddress group = InetAddress.getByName(MULTICAST_ADDRESS);
-                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, group, PORT);
-                socket.send(packet);
+                synchronized (System.out) {
+                    System.out.println("Received packet from " + packet.getAddress().getHostAddress() + ":" + packet.getPort() + " with message:");
+                    String message = new String(packet.getData(), 0, packet.getLength());
+                    System.out.println(message);
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
