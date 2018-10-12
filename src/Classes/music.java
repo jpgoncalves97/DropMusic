46d78f0fec151e0f3418f625c9c832870e480385
jpgoncalves97 <jpmg411@gmail.com>
2018-10-12 15:25:08 +0100

    Multicast server

--- src/Classes/music.java
+++ src/Classes/music.java
@@ -2,9 +2,16 @@ package Classes;
 
 public class music {
     private boolean publico;
+    private String nome;
 
 
-    public music(boolean publico){
+    public music(String nome, boolean publico){
+        this.nome = nome;
         this.publico = publico;
+    }
+
+    @Override
+    public String toString(){
+        return nome + ";" + (publico == true ? "true" : "false");
     }
 }
--- src/sd/MulticastServer.java
+++ src/sd/MulticastServer.java
@@ -1,7 +1,8 @@
 package sd;
 
-import Classes.user;
+import Classes.*;
 
+import java.io.Console;
 import java.net.*;
 import java.io.IOException;
 import java.util.Enumeration;
@@ -31,8 +32,42 @@ class MulticastServer extends Thread {
             MulticastSocket socket = new MulticastSocket(PORT);  // create socket and bind it
             InetAddress group = InetAddress.getByName(MULTICAST_ADDRESS);
             socket.joinGroup(group);
-            new MulticastReceiverThread(socket).start();
-            new MulticastSenderThread(socket).start();
+            new MulticastReceiverThread(socket, new Runnable() {   public void run() {
+                while (true) {
+                    try {
+                        byte[] buffer = new byte[1024];
+                        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                        socket.receive(packet);
+                        String[] msg = new String(packet.getData(), packet.getOffset(), packet.getLength()).split(";");
+                        for (String s : msg) {
+                            System.out.println(s);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }}).start();
+            new MulticastSenderThread(socket, new Runnable () {
+                public void run() {
+                    InetAddress group;
+                    try {
+                        group = InetAddress.getByName(MulticastServer.MULTICAST_ADDRESS);
+                        while (true) {
+                            try {
+                                music m = new music("garagem da vizinha", true);
+                                String msg = m.toString();
+                                byte[] buffer = msg.getBytes();
+                                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, group, MulticastServer.PORT);
+                                socket.send(packet);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    } catch (UnknownHostException e) {
+                        e.printStackTrace();
+                    }
+
+                }}).start();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -44,62 +79,21 @@ class MulticastReceiverThread extends Thread {
 
     private MulticastSocket socket;
 
-    MulticastReceiverThread(MulticastSocket socket) {
+    MulticastReceiverThread(MulticastSocket socket, Runnable runnable) {
+        super(runnable);
         this.socket = socket;
     }
 
-    public void run() {
-        while (true) {
-            try {
-                byte[] buffer = new byte[1024];
-                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-                socket.receive(packet);
-                String[] msg = new String(packet.getData(), packet.getOffset(), packet.getLength()).split(";");
-                for (String s : msg) {
-                    System.out.println(s);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public int getHash(){
-        return this.hashCode();
-    }
 }
 
 class MulticastSenderThread extends Thread {
 
     private MulticastSocket socket;
 
-    MulticastSenderThread(MulticastSocket socket) {
+    MulticastSenderThread(MulticastSocket socket, Runnable runnable) {
+        super(runnable);
         this.socket = socket;
     }
 
-    public void run() {
-        Scanner keyboardScanner = new Scanner(System.in);
-        InetAddress group;
-        try {
-            group = InetAddress.getByName(MulticastServer.MULTICAST_ADDRESS);
-            while (true) {
-                try {
-                    String readKeyboard = keyboardScanner.nextLine();
-                    byte[] buffer = readKeyboard.getBytes();
-                    DatagramPacket packet = new DatagramPacket(buffer, buffer.length, group, MulticastServer.PORT);
-                    socket.send(packet);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public int getHash(){
-        return this.hashCode();
-    }
 }
 
