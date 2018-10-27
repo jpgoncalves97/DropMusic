7b44a08e1255ec2d93333d750225ed7ba4a35512
jpgoncalves97 <jpmg411@gmail.com>
2018-10-27 14:23:53 +0100

    Fix multicast

--- src/Classes/playlist.java
+++ src/Classes/playlist.java
@@ -1,29 +1,36 @@
 package Classes;
 
+import javax.print.DocFlavor;
 import java.io.Serializable;
 import java.util.ArrayList;
 
 public class playlist implements Serializable {
     private static final long serialVersionUID = 1L;
     private ArrayList<music> musicas;
-    private boolean privacy;
-    public playlist(){
+    private boolean privado;
+    private String nome;
+    private String owner;
+    public playlist(String nome, boolean privado, String owner){
+        this.nome = nome;
+        this.privado = privado;
+        this.owner = owner;
         musicas = new ArrayList<>();
     }
 
+    public String getOwner(){ return owner; }
+    public String getNome(){ return nome; }
     public ArrayList<music> getMusicas() {
         return musicas;
     }
 
-    public boolean isPrivacy() {
-        return privacy;
+    public boolean isPrivado() {
+        return privado;
     }
 
-    public void setPrivacy(boolean privacy) {
-        this.privacy = privacy;
+    public void setPrivado(boolean privado) {
+        this.privado = privado;
     }
-
-    public void addMusicas(music musica) {
+    public void addMusica(music musica) {
         this.musicas.add(musica);
     }
 }
